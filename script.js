c6448b755ce91dbd0b0bb1bc20c9b9a818949119
gargasaha <sahagarga000@gmail.com>
2023-05-14 14:49:01 +0530

    final commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let a='red';
+// if(a=='red'){
+//     console.log("true");
+// }
+
+// switch(a){
+//     case "red":
+//         console.log("red");
+//         break;
+// }
+
+let i=0;
+while(i<=10){
+    console.log(i);
+    i++;
+}
